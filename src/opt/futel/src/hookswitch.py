134aa6c40efc51153038155b88ef0d414cf63bfc
Karl Anderson <kra@monkey.org>
2019-12-05 10:00:39 -0800

    backgroud and better termination

--- src/opt/futel/src/hookswitch.py
+++ src/opt/futel/src/hookswitch.py
@@ -3,30 +3,57 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import datetime
 
 PIN=21
 
 GPIO.setmode(GPIO.BCM)          # pin numbering?
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+record_child = None
+
 PLAY_INTRO_CMD = [
-    'aplay', '--device=plughw:1,0', '/usr/share/sounds/alsa/Front_Center.wav']
+    'aplay',
+    '--device=plughw:1,0',
+    '/usr/share/sounds/alsa/Front_Center.wav']
+
+# XXX time limit
+RUN_RECORD_CMD = [
+    'arecord',
+    '--device=plughw:1,0',
+    '--format',
+    'S16_LE',
+    '--rate',
+    '44100',
+    '-c1',
+    '--process-id-file',
+    '/opt/futel/var/pid',
+    '/opt/futel/var/test.wav']
+
+# def filename():
+#     return datetime.datetime.now().isoformat()
 
 def play_intro():
     subprocess.call(PLAY_INTRO_CMD)
 
 def record():
-    pass
+    global record_child
+    record_child = subprocess.Popen(RUN_RECORD_CMD)
 
-def signal_record():
-    pass
+def terminate_record():
+    global record_child
+    if record_child is None:
+        print("no recording child")
+    else:
+        record_child.terminate()
+    record_child = None
 
 def hookswitch_up():
     play_intro()
     record()
 
 def hookswitch_down():
-    signal_record()
+    terminate_record()
 
 def button_callback(channel):
     if GPIO.input(PIN):
@@ -36,8 +63,9 @@ def button_callback(channel):
         print("falling")        # push
         hookswitch_down()
 
-GPIO.add_event_detect(PIN,GPIO.BOTH,callback=button_callback)
+GPIO.add_event_detect(PIN, GPIO.BOTH, callback=button_callback)
 
 while True:
     print("cycle")
-    time.sleep(60)
+    # XXX should check and stop recording if hookswitch released
+    time.sleep(1)
