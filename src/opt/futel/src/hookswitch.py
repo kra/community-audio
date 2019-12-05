d2390805bd170ceb735108620edf36377866ce02
Karl Anderson <kra@monkey.org>
2019-12-04 21:39:43 -0800

    runnable poc

--- src/opt/futel/src/hookswitch.py
+++ src/opt/futel/src/hookswitch.py
@@ -2,23 +2,42 @@
 
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
 PIN=21
 
-GPIO.setmode(GPIO.BCM)
-
+GPIO.setmode(GPIO.BCM)          # pin numbering?
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+PLAY_INTRO_CMD = [
+    'aplay', '--device=plughw:1,0', '/usr/share/sounds/alsa/Front_Center.wav']
+
+def play_intro():
+    subprocess.call(PLAY_INTRO_CMD)
+
+def record():
+    pass
+
+def signal_record():
+    pass
+
+def hookswitch_up():
+    play_intro()
+    record()
+
+def hookswitch_down():
+    signal_record()
 
 def button_callback(channel):
     if GPIO.input(PIN):
-        print("rising")
+        print("rising")         # release
+        hookswitch_up()
     else:
-        print("falling")
-    print("Button was pushed!")
+        print("falling")        # push
+        hookswitch_down()
 
 GPIO.add_event_detect(PIN,GPIO.BOTH,callback=button_callback)
 
 while True:
     print("cycle")
-    time.sleep(60 * 60)
+    time.sleep(60)
